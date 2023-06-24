a738f02011b85aaaf7db75148515f59d492ce61d
darlene9497 <darlene.nandabi@student.moringaschool.com>
2023-06-24 14:46:54 +0300

    Added programs

---
+++ src/components/programs/Programs.jsx
@@ -0,0 +1,28 @@
+import './programs.css'
+import { programsData } from '../../data/programsData.js'
+import arrow from'../../assets/rightArrow.png'
+
+export default function Programs() {
+  return (
+    <div className='programs'>
+        <div className="programs-header">
+            <span className='stroke-text'>Explore our</span>
+            <span>Programs</span>
+            <span className='stroke-text'>tailored for you</span>
+        </div>
+        <div className="program-categories">
+            {programsData.map((program) => (
+            <div className="category">
+                {program.image}
+                <span>{program.heading}</span>
+                <span>{program.details}</span>
+            <div className="join-now">
+                <span>Join Now</span>
+                <img src={arrow} alt="" />
+            </div>
+            </div>
+          ))}
+        </div>
+    </div>
+  )
+}
