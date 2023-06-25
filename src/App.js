a384c99eb02d9de774725c16b0637eca9bb3ce2a
darlene9497 <darlene.nandabi@student.moringaschool.com>
2023-06-25 12:48:34 +0300

    Added reasons why

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Hero from './components/hero/Hero';
 import Programs from './components/programs/Programs';
+import Reason from './components/reason/Reason';
 
 function App() {
   return (
     <div className="App">
       <Hero />
       <Programs />
+      <Reason />
     </div>
   );
 }
---
+++ src/components/reason/Reason.jsx
@@ -0,0 +1,44 @@
+import './reason.css'
+import img1 from '../../assets/jump.jpeg'
+import img2 from '../../assets/kettlebell.jpg'
+import img3 from '../../assets/fitman1.jpeg'
+import img4 from '../../assets/fitgirl1.jpeg'
+import tick from '../../assets/tick.png'
+
+export default function Reason() {
+  return (
+    <div className='reason'>
+        <div className="left-r">
+            <img src={img1} alt="" />
+            <img src={img3} alt="" />
+            <img src={img2} alt="" />
+            <img src={img4} alt="" />
+        </div>
+        <div className="right-r">
+            <span>Several considerations</span>
+            <div>
+                <span className="stroke-text">why </span>
+                <span>choose us?</span>
+            </div>
+            <div className="details-r">
+                <div>
+                    <img src={tick} alt="" />
+                    <span>Convenient home training fitness program</span>
+                </div>
+                <div>
+                    <img src={tick} alt="" />
+                    <span>Customized workout programs designed by a reliable instructor</span>
+                </div>
+                <div>
+                    <img src={tick} alt="" />
+                    <span>Time flexibility that suits your schedule</span>
+                </div>
+                <div>
+                    <img src={tick} alt="" />
+                    <span>Affordable and budget-friendly option for achieving your fitness goals</span>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
